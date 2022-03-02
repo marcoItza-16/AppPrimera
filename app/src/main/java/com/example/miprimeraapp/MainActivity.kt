1dae7427f7eb883a1088e79664ad938f34f647d5
marcoestrada-17 <javier.itza.est@qroo.nuevaescuela.mx>
2022-03-02 00:42:41 -0500

    Primer Commit

--- app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.miprimeraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bSaludar = findViewById<Button>(R.id.bSaludar)
+        val etNombre = findViewById<EditText>(R.id.etNombre)
+        val cbDev = findViewById<CheckBox>(R.id.cbDeveloper)
+
+        bSaludar.setOnClickListener(View.OnClickListener {
+            if(validaDato()) {
+                if (cbDev.isChecked) {
+                    Toast.makeText(this, "Bienvenido," + etNombre.text + ", se que eres un desarrollador", Toast.LENGTH_LONG).show()
+                } else{
+                    Toast.makeText(this, "Bienvenido, " + etNombre.text, Toast.LENGTH_LONG).show()
+                }
+
+            }else{
+                Toast.makeText(this, "Escribe tu nombre para saludar :/", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+    }
+    fun validaDato(): Boolean {
+
+        val etNombre = findViewById<EditText>(R.id.etNombre)
+        val nombreUsuario = etNombre.text
+
+        if(nombreUsuario.isNullOrEmpty()) {
+            return false
+
+        }
+        return true
     }
 }
